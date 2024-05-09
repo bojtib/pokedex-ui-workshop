ee38cc19b6ba40ca1a71be9a81e12c5e13c09a4c
Bence Boejti <bojti@amazon.com>
2024-05-09 11:07:13 +0200

    Full solution.

--- src/app/pokemon/[pokemon_id]/pokemon.tsx
+++ src/app/pokemon/[pokemon_id]/pokemon.tsx
@@ -1,12 +1,25 @@
 import Pokemon from "@/model/pokemon";
-import { Row, Col, Container, Image } from 'react-bootstrap';
+import { Row, Col, Container, Image, ProgressBar, Card, ListGroup, Badge } from 'react-bootstrap';
 
-type Props ={
+type Props = {
     pokemon: Pokemon;
 }
 
-export default function PokemonComponent(props : Props) {
-    const {pokemon} = props;
+export default function PokemonComponent(props: Props) {
+    const { pokemon } = props;
+
+    const getFamilyTag = (pokemon: Pokemon, evolution: string) => {
+        if (evolution === pokemon.devolution) {
+          return <Badge bg="danger">Devolution</Badge>
+        }
+        if (evolution === pokemon.pokemonName) {
+          return <Badge bg="primary">Current</Badge>
+        }
+        if (evolution === pokemon.evolution) {
+          return <Badge bg="success">Evolution</Badge>
+        }
+        return <></>
+      }
 
     return (
         <Container>
@@ -15,10 +28,36 @@ export default function PokemonComponent(props : Props) {
             </Row>
             <Row>
                 <Col>
-                    <Image src={pokemon.mainImage} thumbnail />
-                </Col>
+                    <Image src={pokemon.mainImage} thumbnail /></Col>
                 <Col>
-                    Pokémon Properties
+                    <Row>
+                        Speed: <ProgressBar variant="info" now={pokemon.speed} label={pokemon.speed} />
+                    </Row>
+                    <Row>
+                        Health points: <ProgressBar variant="danger" now={pokemon.healthPoints} label={pokemon.healthPoints} />
+                    </Row>
+                    <Row>
+                        Attack: <ProgressBar variant="warning" now={pokemon.attack} label={pokemon.attack} />
+                    </Row>
+                    <Row>
+                        Defense: <ProgressBar variant="success" now={pokemon.defense} label={pokemon.defense} />
+                    </Row>
+                    <Row className="p-2">
+                        <Card>
+                            <Card.Header>Pokemon type</Card.Header>
+                            <ListGroup variant="flush">
+                                {pokemon.pokemonType.map(type => <ListGroup.Item>{type}</ListGroup.Item>)}
+                            </ListGroup>
+                        </Card>
+                    </Row>
+                    <Row className="p-2">
+                        <Card>
+                            <Card.Header>Evaluation family</Card.Header>
+                            <ListGroup variant="flush">
+                                {pokemon.evolutionFamily.map(evolution => <ListGroup.Item>{evolution} {getFamilyTag(pokemon, evolution)}</ListGroup.Item>)}
+                            </ListGroup>
+                        </Card>
+                    </Row>
                 </Col>
             </Row>
         </Container>
